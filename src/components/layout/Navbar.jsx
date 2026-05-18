ea4bb7bb1a555ef876760eb256f7bc6029c43322
Dhanush Balakrishnan <132364060+Dhanush102003@users.noreply.github.com>
2026-05-18 21:50:56 +0530

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
             </button>
 
              <a
-              href="https://docs.google.com/spreadsheets/d/1pDNizeUwykCIokaQVPRSIxlW0nejYKIwklgpRnZAZog/edit?usp=sharing"
+              href="https://docs.google.com/spreadsheets/d/1zn2scuRsu1p-57HoxPsTVWcju_uMjGF3/edit?usp=sharing&ouid=112065794432412324846&rtpof=true&sd=true"
               target="_blank"
               rel="noopener noreferrer"
               className={`p-2 rounded-xl transition-all duration-300 ${isDark ? 'hover:bg-white/10 text-gray-300' : 'hover:bg-gray-100 text-gray-600'}`}
